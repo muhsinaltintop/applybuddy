09b41dc015bd988b6c6532cae6ec06704149f05f
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-09-11 22:24:47 +0100

    inital commit fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-xl font-bold">Apply Buddy</h1>
         <h2 className="text-lg font-bold">To-Do List:</h2>
-        <ol className="">
-          <li className="">Create Register</li>
-          <li className="">Create Login</li>
+        <ol className="list-decimal">
+          <li>Create Register</li>
+          <li>Create Login</li>
         </ol>
 
         
